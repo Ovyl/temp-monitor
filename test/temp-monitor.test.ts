79eef25981ba7a1ca1d75dab48a0495cdeaea639
meatherly <michael@ovyl.io>
2022-01-11 11:20:27 -0600

    Initial commit

---
+++ test/temp-monitor.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TempMonitor from '../lib/temp-monitor-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/temp-monitor-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TempMonitor.TempMonitorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
